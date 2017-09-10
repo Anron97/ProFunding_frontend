0648b38e57d2c61a3e7e9d7943f01a8c9f98458b
Андрей <Anron2012@mail.ru>
2017-09-10 17:58:44 +0300

    first version of Finansal Goal

--- src/app/models/finansalGoal.ts
+++ src/app/models/finansalGoal.ts
@@ -2,4 +2,10 @@ export class FinansalGoal {
     title: string;
     description: string;
     cost: number;
+
+    constructor(title: string, description: string, cost: number) {
+        this.title = title;
+        this.description = description;
+        this.cost = cost;
+    }
 }
